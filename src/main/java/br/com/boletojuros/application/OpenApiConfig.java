9e4b85f93345f92bc92770d80de538c87aff193c
eskokado <eskokado@gmail.com>
2023-11-11 18:36:30 -0300

    feat: create OpenApiConfig

---
+++ src/main/java/br/com/boletojuros/application/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package br.com.boletojuros.application;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API juros boleto")
+                        .description("API para calcular juros de boleto vencido")
+                        .contact(new Contact()
+                                .name("Edson Shideki Kokado")
+                                .email("eskokado@gmail.com"))
+                        .version("1.0.0"));
+    }
+}
