eabd4f0702d38346bd41078cc2a987ad3a884516
eskokado <eskokado@gmail.com>
2023-11-11 18:38:07 -0300

    feat: add Docs OpenApi

--- src/main/java/br/com/boletojuros/adapter/http/CalculoBoletoController.java
+++ src/main/java/br/com/boletojuros/adapter/http/CalculoBoletoController.java
@@ -1,9 +1,12 @@
 package br.com.boletojuros.adapter.http;
 
+import br.com.boletojuros.adapter.datasource.mapper.BoletoCalculoMapper;
 import br.com.boletojuros.adapter.http.dto.CalculoBoletoRequest;
 import br.com.boletojuros.adapter.http.dto.CalculoBoletoResponse;
-import br.com.boletojuros.adapter.http.mapper.CalculoBoletoMapper;
+import br.com.boletojuros.adapter.http.mapper.BoletoCalculadoMapper;
 import br.com.boletojuros.core.port.in.CalculoBoletoPort;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,14 +20,15 @@ import javax.validation.Valid;
 public class CalculoBoletoController {
 
     private final CalculoBoletoPort calculoBoletoPort;
-    private final CalculoBoletoMapper mapper;
+    private final BoletoCalculadoMapper mapper;
 
-    public CalculoBoletoController(CalculoBoletoPort calculoBoletoPort, CalculoBoletoMapper mapper) {
+    public CalculoBoletoController(CalculoBoletoPort calculoBoletoPort, BoletoCalculadoMapper mapper) {
         this.calculoBoletoPort = calculoBoletoPort;
         this.mapper = mapper;
     }
 
-    @PostMapping("/calculo")
+    @PostMapping(path = "/calculo", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Calcular juros de um boleto")
     public ResponseEntity<CalculoBoletoResponse> calcularBoleto(@Valid @RequestBody CalculoBoletoRequest boleto) {
         var boletoCalculado = calculoBoletoPort.executar(boleto.getCodigo(), boleto.getDataPagamento());
         return ResponseEntity.ok(mapper.toDTO(boletoCalculado));
